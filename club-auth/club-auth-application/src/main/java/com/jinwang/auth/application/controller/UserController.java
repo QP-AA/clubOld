a637133252b7c59c2cfb092d8e88413d25d3975b
QP-E <1366849802@qq.com>
2024-10-15 22:53:30 +0800

    userController

---
+++ club-auth/club-auth-application/src/main/java/com/jinwang/auth/application/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.jinwang.auth.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
+
+public class UserController {
+
+}
